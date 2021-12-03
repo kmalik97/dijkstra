a927042d0321bc837f14a405a867cb1cfbab21ea
Khurram Malik <khurrammalik97@gmail.com>
2021-12-03 01:59:27 -0500

    good enough

--- src/components/Node.js
+++ src/components/Node.js
@@ -2,9 +2,11 @@ import React from 'react'
 import {useState,useEffect} from 'react';
 import './Node2.css'; 
 
-export default function Node({node}) {
+export default function Node({node,addWall,mouseDown,updateStart,moveStart,updateEnd,moveEnd}) {
     
     const [classState,setClassState] = useState('node');
+
+
 
     useEffect(() => {
         
@@ -24,10 +26,25 @@ export default function Node({node}) {
 
     }, []) 
       
-    
+    const doSomething = () => {
+        console.log(mouseDown)
+        if(mouseDown){
+            console.log("mouse over");
+            addWall(node.row,node.col);    
+        }
+    }
+
+    const updateToStartFinish = () => {
+        if(moveStart){
+            updateStart(node.row,node.col); 
+        }
+        if(moveEnd){
+            updateEnd(node.row,node.col);
+        }
+    }
     
     return (
-        <div id={`node-${node.row}-${node.col}`} className={classState} >
+        <div onMouseDown={() => {updateToStartFinish()}} onMouseOver={()=> {doSomething()}} id={`node-${node.row}-${node.col}`} className={classState} >
         
         </div>
     )
--- src/components/PathPlanningVisualizer.js
+++ src/components/PathPlanningVisualizer.js
@@ -4,25 +4,28 @@ import Node from "./Node";
 import "./PathPlanningVisualizer.css";
 import Button from "@mui/material/Button";
 import { dijkstra, getNodesInShortestPathOrder } from "./dijkstra";
-import Slider from "@mui/material/Slider";
 
 import Grid from "@mui/material/Grid";
-import Stack from "@mui/material/Stack";
 
-
-const START_GRID_ROW = 10;
-const START_GRID_COL = 10;
-const END_GRID_ROW = 10;
-const END_GRID_COL = 20;
+// const START_GRID_ROW = 10;
+// const START_GRID_COL = 10;
+// const END_GRID_ROW = 10;
+// const END_GRID_COL = 20;
 
 export default function PathPlanningVisualizer() {
+  const [GRID_ROW, setGridRow] = useState(40);
+  const [GRID_COL, setGridCol] = useState(40);
 
+  const [START_GRID_COL,setStartGridCol] = useState(10);
+  const [START_GRID_ROW,setStartGridRow] = useState(10);
+  const [END_GRID_ROW,setEndGridRow] = useState(20);
+  const [END_GRID_COL,setEndGridCol] = useState(20);
+  const [moveStartClicked,setMoveStartClicked] = useState(false);
+  const [moveEndClicked,setEndClicked] = useState(false); 
 
-
-  const [GRID_ROW, setGridRow] = useState(50);
-  const [GRID_COL, setGridCol] = useState(50);
-    
   const [board, setBoard] = useState([]);
+
+  const [mouseDown, setMouseDown] = useState(false);
 
   const createNode = (col, row) => {
     return {
@@ -90,8 +93,95 @@ export default function PathPlanningVisualizer() {
     }
   };
 
-  
+  const getNewGridWithWallToggled = (row, col) => {
+    const newGrid = board.slice();
+    const node = newGrid[row][col];
 
+    if (node.isStart || node.isEnd) {
+      return;
+    }
+
+    const newNode = {
+      ...node,
+      isWall: !node.isWall,
+    };
+    if (newNode.isWall) {
+      document.getElementById(`node-${node.row}-${node.col}`).className =
+        "node wall";
+    } else {
+      document.getElementById(`node-${node.row}-${node.col}`).className =
+        "node";
+    }
+
+    newGrid[row][col] = newNode;
+    setBoard(newGrid);
+  };
+
+  const moveStart = () => {
+
+    setMoveStartClicked(true); 
+    const newGrid = board.slice();
+    const node = newGrid[START_GRID_ROW][START_GRID_COL];
+    const newNode = {
+        ...node,
+        isStart: false,
+      };
+      newGrid[START_GRID_ROW][START_GRID_COL] = newNode;
+      setBoard(newGrid);
+      document.getElementById(`node-${START_GRID_ROW}-${START_GRID_COL}`).className =
+        "node";
+  };
+    
+  const updateStart = (row,col) => {
+    console.log("Update Start Pressed"); 
+    setMoveStartClicked(false);
+    const newGrid = board.slice();
+    const node = newGrid[row][col];
+
+    const newNode = {
+      ...node,
+      isStart: true,
+    };
+    setStartGridCol(col);
+    setStartGridRow(row);
+    document.getElementById(`node-${node.row}-${node.col}`).className =
+        "node start";
+    newGrid[row][col] = newNode;
+    setBoard(newGrid);
+  }
+
+  const moveEnd = () => {
+
+    setEndClicked(true); 
+    const newGrid = board.slice();
+    const node = newGrid[END_GRID_ROW][END_GRID_COL];
+    const newNode = {
+        ...node,
+        isEnd: false,
+      };
+      newGrid[END_GRID_ROW][END_GRID_COL] = newNode;
+      setBoard(newGrid);
+      document.getElementById(`node-${END_GRID_ROW}-${END_GRID_COL}`).className =
+        "node";
+  };
+    
+  const updateEnd = (row,col) => {
+    console.log("update end"); 
+    setEndClicked(false);
+    const newGrid = board.slice();
+    const node = newGrid[row][col];
+
+    const newNode = {
+      ...node,
+      isEnd: true,
+    };
+    setEndGridCol(col);
+    setEndGridRow(row);
+    document.getElementById(`node-${node.row}-${node.col}`).className =
+        "node finish";
+    newGrid[row][col] = newNode;
+    setBoard(newGrid);
+  }
   return (
     <div>
       <Grid container direction="column" spacing={1}>
@@ -111,25 +201,65 @@ export default function PathPlanningVisualizer() {
               variant="contained"
               color="error"
               onClick={() => {
-                handleButtonClick();
+                window.location.reload(false);
               }}
             >
               Reset Grid
             </Button>
           </Grid>
+          <Grid item>
+            <Button
+              variant="contained"
+              color="success"
+              onClick={() => {
+                moveStart();
+              }}
+            >
+              Move Start
+            </Button>
+          </Grid>
+          <Grid item>
+            <Button
+              variant="contained"
+              color="success"
+              onClick={() => {
+                moveEnd();
+              }}
+            >
+              Move End
+            </Button>
+          </Grid>
         </Grid>
-        
 
         <Grid item>
-          {board.map((row, rowId) => {
-            return (
-              <div className="row" key={rowId}>
-                {row.map((node, colId) => {
-                  return <Node node={node}></Node>;
-                })}
-              </div>
-            );
-          })}
+          <div
+            onMouseDown={() => {
+              setMouseDown(true);
+            }}
+            onMouseUp={() => {
+              setMouseDown(false);
+            }}
+          >
+            {board.map((row, rowId) => {
+              return (
+                <div className="row" key={rowId}>
+                  {row.map((node, colId) => {
+                    return (
+                      <Node
+                        node={node}
+                        addWall={getNewGridWithWallToggled}
+                        mouseDown={mouseDown}
+                        updateStart={updateStart}
+                        moveStart={moveStartClicked}
+                        updateEnd={updateEnd}
+                        moveEnd={moveEndClicked}
+                      ></Node>
+                    );
+                  })}
+                </div>
+              );
+            })}
+          </div>
         </Grid>
       </Grid>
     </div>
